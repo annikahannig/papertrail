22c770de32691828f8dfc63ebf0c95ce01fa13f7
Matthias Hannig <matthias@hannig.cc>
2015-05-20 15:47:03 +0200

    added remote messages...

--- server/models/node.go
+++ server/models/node.go
@@ -2,11 +2,45 @@ package models
 
 import (
 	"log"
+	"net"
 )
 
 type Node struct {
-	Id       string
-	Name     string
-	Hostname string
-	Uname    string
+	Id       string   `json:"id"`
+	Name     string   `json:"name"`
+	Hostname string   `json:"hostname"`
+	Uname    string   `json:"uname"`
+	Addr     net.Addr `json:"addr"`
+}
+
+var ConnectedNodes []*Node
+
+func init() {
+	// Initialize Nodes Slice
+	ConnectedNodes = make([]*Node, 0, 10)
+}
+
+func RegisterNode(node *Node) {
+	ConnectedNodes = append(ConnectedNodes, node)
+	log.Println("[Nodes] Registered connected node id:", node.Id)
+}
+
+func UnregisterNode(node *Node) {
+	// Find node pos
+	pos := -1
+	for p, n := range ConnectedNodes {
+		if n == node {
+			pos = p
+			break
+		}
+	}
+
+	// Splice connected nodes
+	if pos != -1 {
+		copy(ConnectedNodes[pos:], ConnectedNodes[pos+1:])
+		ConnectedNodes[len(ConnectedNodes)-1] = nil
+		ConnectedNodes = ConnectedNodes[:len(ConnectedNodes)-1]
+	}
+
+	log.Println("[Nodes] Removed connected node id:", node.Id)
 }
---
+++ server/remote/messages.go
@@ -0,0 +1,5 @@
+package remote
+
+const (
+	M_REGISTER_NODE = 0
+)
