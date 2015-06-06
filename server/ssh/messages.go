2d747f51befd5ad936f31f93bde096d644d53cb2
Matthias Hannig <matthias@hannig.cc>
2015-06-06 17:28:25 +0200

    defined server messasge

--- server/ssh/messages.go
+++ server/ssh/messages.go
@@ -3,7 +3,37 @@ package sshServer
 const (
 	M_SERVER_INFO = iota
 	M_REGISTER_NODE
-	M_NODE_INFO
+	M_REGISTER_NODE_RESPONSE
 	M_PRINT_JOB
-	M_PRINT_JOB_RESPONSE
+	M_JOB_RESPONSE
 )
+
+type MsgServerInfo struct {
+	Version          string `json:"version"`
+	Motd             string `json:"motd"`
+	ConnectedNodes   int    `json:"connectedNodes"`
+	ConnectedClients int    `josn:"connectedClients"`
+}
+
+type MsgRegisterNode struct {
+	Name     string `json:"name"`
+	Hostname string `json:"hostname"`
+	Uname    string `json:"uname"`
+}
+
+type MsgRegisterNodeResponse struct {
+	Success bool   `json:"success"`
+	Id      string `json:"id"`
+}
+
+type MsgPrintJob struct {
+	Id      string `json:"id"`
+	Content string `json:"content"`
+}
+
+type MsgJobResponse struct {
+	Id      string `json:"id"`
+	Status  string `json:"status"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
