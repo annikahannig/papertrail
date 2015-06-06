96498cfd8e1a140a59bb64a517948c04132893fd
Matthias Hannig <matthias@hannig.cc>
2015-06-07 00:38:04 +0200

    added message encoding

--- server/ssh/messages.go
+++ server/ssh/messages.go
@@ -1,5 +1,10 @@
 package sshServer
 
+import (
+	"encoding/json"
+	"golang.org/x/crypto/ssh"
+)
+
 const (
 	M_SERVER_INFO = iota
 	M_REGISTER_NODE
@@ -36,4 +41,41 @@ type MsgJobResponse struct {
 	Status  string `json:"status"`
 	Code    int    `json:"code"`
 	Message string `json:"message"`
+}
+
+/**
+ * Encode / Decode messages
+ */
+
+func EncodeMessage(id byte, message interface{}) ([]byte, error) {
+	var result []byte
+
+	// Encode json message
+	jsonMessage, err := json.Marshal(message)
+	if err != nil {
+		return nil, err
+	}
+
+	// Append message type / id and length
+	result = append(result, id)
+
+	// Encode message length 4 bytes big endian
+	size := uint32(len(jsonMessage))
+	result = append(result,
+		byte((size&0xff000000)>>24),
+		byte((size&0x00ff0000)>>16),
+		byte((size&0x0000ff00)>>8),
+		byte((size&0x000000ff)>>0))
+
+	// Append Json encoded message
+	result = append(result, jsonMessage...)
+
+	// Append network flush
+	result = append(result, '\n', '\r')
+
+	return result, err
+}
+
+func ReadMessage(channel ssh.Channel) (int, *interface{}) {
+	return M_SERVER_INFO, nil
 }
