77b1fa6fe3ed640c9b57a0708080fa9977aacde4
Matthias Hannig <matthias@hannig.cc>
2015-06-09 00:50:34 +0200

    Fixed type problem with decoded messages.

--- server/messages/messages.go
+++ server/messages/messages.go
@@ -73,11 +73,10 @@ func Encode(id byte, message interface{}) ([]byte, error) {
 	return result, err
 }
 
-func Receive(channel ssh.Channel) (uint8, *interface{}, error) {
+func Receive(channel ssh.Channel) (uint8, interface{}, error) {
 	var (
 		size uint32
 		id   uint8
-		msg  interface{}
 	)
 
 	mid := make([]byte, 1)
@@ -108,20 +107,31 @@ func Receive(channel ssh.Channel) (uint8, *interface{}, error) {
 		return 255, nil, err
 	}
 
+	// This is not elegant.
 	switch id {
 	case M_SERVER_INFO:
-		msg = MsgServerInfo{}
+		msg := MsgServerInfo{}
+		err = json.Unmarshal(payload, &msg)
+		return id, msg, err
 	case M_REGISTER_NODE:
-		msg = MsgRegisterNode{}
+		msg := MsgRegisterNode{}
+		err = json.Unmarshal(payload, &msg)
+		return id, msg, err
 	case M_REGISTER_NODE_RESPONSE:
-		msg = MsgRegisterNodeResponse{}
+		msg := MsgRegisterNodeResponse{}
+		err = json.Unmarshal(payload, &msg)
+		return id, msg, err
 	case M_PRINT_JOB:
-		msg = MsgPrintJob{}
+		msg := MsgPrintJob{}
+		err = json.Unmarshal(payload, &msg)
+		return id, msg, err
 	case M_JOB_RESPONSE:
-		msg = MsgJobResponse{}
+		msg := MsgJobResponse{}
+		err = json.Unmarshal(payload, &msg)
+		return id, msg, err
 	}
+	// In fact this looks very ugly.
+	// There has to be a better way. :-/
 
-	// Decode json message payload
-	err = json.Unmarshal(payload, &msg)
-	return id, &msg, err
+	return 255, nil, nil
 }
